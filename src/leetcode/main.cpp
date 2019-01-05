eeda0c4114cdc9adedae76f4ca32f8e89e3cb87c
otahsu <otamuphysic@gmail.com>
2019-01-05 17:15:32 +0800

    use std library in header.

--- src/leetcode/main.cpp
+++ src/leetcode/main.cpp
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <stdio.h> //preprocessor: do copy/paste for header.
 #include <iostream>
+#include <string> //for string
+#include <algorithm> //for reverse
 using namespace std;
+
+
+class Solution {
+public:
+    string reverseString(string s) {
+        reverse(begin(s), end(s));
+        return s;
+    }
+};
+
 int main(){
     printf("");
     //std::cout << "h e" <std::endl;
