ce34b002c60bb046fcb34bba4bca8436f183b55e
otahsu <otamuphysic@gmail.com>
2019-01-05 23:46:09 +0800

    enum 2 index compositions from 0~3(16 pairs).

---
+++ src/leetcode/two_sum/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i=0; i < 4; i++) {
+        for (int j=0; j < 4; j++) {
+           cout << i << ", "<< j << endl; 
+        }
+    }
+    return 0;
+}
+
+
+// vim: set ft=cpp ff=unix fenc=utf8 ai et nu sw=4 ts=4 tw=85:
