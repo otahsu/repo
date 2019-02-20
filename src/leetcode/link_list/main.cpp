bc4b1297ff83ecd410a7463466a2dfe29c3fbb59
otahsu <otamuphysic@gmail.com>
2019-02-20 16:39:40 +0800

    an easy sample-code for the container of and its iterator.

---
+++ src/leetcode/link_list/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <forward_list>
+#include <iostream>
+
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+};
+
+
+int main(){
+    int *r = NULL; //C macro NULL==0 
+    int *q = nullptr; //C++'s null pointer
+    
+    forward_list<int> a = {1,2,3};
+    for (auto p = begin(a); p !=end(a); p++){
+        cout << *p << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+// vim: set ft=cpp ff=unix fenc=utf8 ai et nu sw=4 ts=4:
